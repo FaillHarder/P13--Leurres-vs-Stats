e2ada41638b5c92323c7c6f3f2ee19e2cffc8484
unknown <fdlcm974@gmail.com>
2022-02-11 15:15:13 +0100

    suppression de 8 erreurs flake8

--- tests/home/test_home_views.py
+++ tests/home/test_home_views.py
@@ -2,12 +2,13 @@ from app.home import views
 
 from django.test import TestCase, RequestFactory
 
+
 class TestHomeView(TestCase):
 
     def setUp(self):
         self.factory = RequestFactory()
         return super().setUp()
-    
+
     def test_index_view(self):
         request = self.factory.get('/')
         response = views.index(request)
