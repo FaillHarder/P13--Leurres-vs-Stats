41e6c518c5b8c7f13354c786a90fccfc6674751d
unknown <fdlcm974@gmail.com>
2022-03-21 10:11:43 +0100

    finalisation des tests de la commande personnalisée

---
+++ app/adddata/forms.py
@@ -0,0 +1,9 @@
+from django.forms import ModelForm
+
+from app.adddata.models import CatchFish
+
+
+class CatchFishForm(ModelForm):
+    class Meta:
+        model = CatchFish
+        fields = ["lure", "color_lure", "sky_state", "water_state"]
--- app/adddata/urls.py
+++ app/adddata/urls.py
@@ -1,6 +1,6 @@
-# from django.urls import path
-# from app.adddata import views
+from django.urls import path
+from app.adddata import views
 
-# urlpatterns = [
-
-# ]
+urlpatterns = [
+    path('catchfish', views.catchfish, name="catchfish")
+]
