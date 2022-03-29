2e35b325574e17789d1a8dc77125ff45fd4b0277
unknown <fdlcm974@gmail.com>
2022-03-29 21:33:24 +0200

    modification du projet. suppression des skystate/waterstate choices = remplacé par les tables SkyState/WaterState. modification des classes AllStatsByChoices/FishingStats + maj des tests

--- app/adddata/admin.py
+++ app/adddata/admin.py
@@ -1,9 +1,11 @@
-from app.adddata.models import Color, Lure, CatchFish
-
 from django.contrib import admin
+
+from app.adddata.models import Color, Lure, CatchFish, SkyState, WaterState
 
 
 # Register your models here.
 admin.site.register(Color)
 admin.site.register(Lure)
 admin.site.register(CatchFish)
+admin.site.register(SkyState)
+admin.site.register(WaterState)
--- app/fishingstats/utils/generate_stats_choices.py
+++ app/fishingstats/utils/generate_stats_choices.py
@@ -1,17 +1,17 @@
 from ..create_stats import FishingStats
-from app.adddata.models import CatchFish
+from app.adddata.models import SkyState, WaterState
 
 
 class AllStatsByChoices():
 
-    sky_states = CatchFish.SKY_CHOICES
-    water_states = CatchFish.WATER_CHOICES
+    sky_states = SkyState.objects.all()
+    water_states = WaterState.objects.all()
 
     def choices_list(self):
         choices_list = []
         for sky_state in self.sky_states:
             for water_state in self.water_states:
-                choices_list.append([sky_state[1], water_state[1]])
+                choices_list.append([sky_state.name, water_state.name])
         return choices_list
 
     def generate_all(self):
@@ -20,7 +20,7 @@ class AllStatsByChoices():
         for list in choices_list:
             all_stats_list.append(
                 [
-                    f"Classement par condition météo. Ciel: {list[0]}/ Eau: {list[1]}",
+                    list[0], list[1],
                     FishingStats().top_lures_and_colors_by_states(list[0], list[1])
                 ]
             )
